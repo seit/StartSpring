de8da209c406bb0419dd28afdbcaadebfde54e80
Masanobu Uenaka <uent192837465@gmail.com>
2019-02-16 20:24:19 +0900

    Add addViewControllers following 5.3.6

--- src/main/java/com/uent192837465/startapp/WebMvcConfig.java
+++ src/main/java/com/uent192837465/startapp/WebMvcConfig.java
@@ -3,6 +3,7 @@ package com.uent192837465.startapp;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -14,6 +15,11 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
 	@Override
 	public void configureViewResolvers(ViewResolverRegistry registry) {
 		registry.jsp();
+	}
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("fuga/").setViewName("views/fuga");
 	}
 
 }
