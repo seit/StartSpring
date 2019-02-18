9f256756fa98e30cda4a8f7e66323d1453e63d9d
Masanobu Uenaka <uent192837465@gmail.com>
2019-02-18 18:03:50 +0900

    Add initBinder for input null check

--- src/main/java/com/uent192837465/startapp/EchoController.java
+++ src/main/java/com/uent192837465/startapp/EchoController.java
@@ -4,9 +4,12 @@ import javax.validation.Valid;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -18,13 +21,19 @@ public class EchoController {
 
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+		// @NotNullのバリデーションチェックは、未入力は""(空文字)として扱うため、Nullチェックに引っかからない。
+		// StringTrimmerEditorをtrueにすることで、""(空文字)をNullに変換してくれる。
+		binder.registerCustomEditor(String.class, new StringTrimmerEditor(true));
+	}
+
 	@RequestMapping(method = RequestMethod.GET)
 	public String viewInput(Model model){
 		EchoForm form = new EchoForm();
 		model.addAttribute(form);
 		return "views/echo/input";
 	}
-
 
 	@RequestMapping(method = RequestMethod.POST)
 	public String echo(@Valid EchoForm form, BindingResult result) {
--- src/main/java/com/uent192837465/startapp/form/EchoForm.java
+++ src/main/java/com/uent192837465/startapp/form/EchoForm.java
@@ -2,15 +2,14 @@ package com.uent192837465.startapp.form;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-
-import org.hibernate.validator.constraints.NotEmpty;
 
 public class EchoForm implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
-	@NotEmpty
+	@NotNull
 	@Size(max=10)
 	private String text;
 
