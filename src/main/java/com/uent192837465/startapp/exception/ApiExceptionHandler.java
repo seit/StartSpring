f3d42e67b970a59c609a0706a16b50cdcfffec8d
Masanobu Uenaka <uent192837465@gmail.com>
2019-02-23 10:52:13 +0900

    フレームワークの例外処理を行うためにApiExceptionHandlerクラスを追加

---
+++ src/main/java/com/uent192837465/startapp/exception/ApiExceptionHandler.java
@@ -0,0 +1,7 @@
+package com.uent192837465.startapp.exception;
+
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
+
+}
